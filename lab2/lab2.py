29743f6bec20a1b28c48231ecee2ae8d24f3ed0d
1160300314 <1160300314@stu.hit.edu.cn>
2018-10-19 09:30:25 +0800

    增加西瓜书样例

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -6,6 +6,37 @@ from scipy.stats import binom
     1.首先不生成数据，利用已有的数据(如西瓜书上的例子)先进行测试
     2.尽量使用lab1中已经造好的轮子进行
  """
+def loadWaterMelonData():
+    dataSet =  np.loadtxt("./watermelon-3.3.csv",delimiter=",")
+    x = dataSet[:,1:3]
+    y = dataSet[:,3]
+    return x, y
+
+def loss(x, y, beta):
+    sum = 0
+    for i in range(len(beta)):
+        sum += (-y[i] * beta @ x[i] + np.math.log(1 + np.math.exp(beta @ x[i])))
+    return sum
+def sigmod(x, beta):
+    return 1.0 / (1.0 + np.math.exp(beta @ x))
+def derivative_beta(x, y, beta):
+    m,n = x.shape
+    ans = np.zeros(n)
+    for i in range(m):
+        temp = y[i] - (1 - sigmod(x[i], beta))
+        for j in range(n):
+            ans[j] += x[i][j] * temp
+    return -1 * ans  # TODO 看西瓜书3.30式 推导一下导数式向量还是标量
+x, y = loadWaterMelonData()
+print(x)
+print(y)
+m,n = np.shape(x)
+x = np.c_[np.ones(m),x]
+print(x)
+beta = np.zeros(n+1)
+print(beta)
+print(derivative_beta(x, y, beta))
+print(loss(x,y,beta))
 
 
 # def generateData(number, meanPos, meanNeg, scale1=0.1, scale2=0.2):
