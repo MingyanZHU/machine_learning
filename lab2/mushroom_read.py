becc517e89a7fd187130fb5428470bfc17b4fe9a
1160300314 <1160300314@stu.hit.edu.cn>
2018-10-20 11:12:36 +0800

    完成mushrooms.csv的预处理 使用了sklearn

--- lab2/mushroom_read.py
+++ lab2/mushroom_read.py
@@ -1,31 +1,18 @@
 import numpy as np
 import pandas as pd
-
-dataset = pd.read_csv("./mushrooms.csv")
-print(dataset.head())
-
-print(dataset.isnull().sum())
-print(dataset['odor'])
-
-x = dataset.drop('class', axis=1)
-y = dataset['class']
-print(x.head())
-print(y.head())
-# houseprice['MSZoning']=houseprice['MSZoning'].map({'RL':1,'RM':2,'RR':3,}).astype(int)
-# 将MSZoning中的字符串变成对应的数字表示
-# x['cap-shape'] = x['cap-shape'].map({''})
-# print(type(x))
-x = pd.get_dummies(x)
-print(x.head())
-
 from sklearn import preprocessing
-xnor = preprocessing.normalize(x)
-xscale = preprocessing.scale(x)
-print(xnor.shape)
-print(xscale)
-
-# TODO 
-# 将mushrooms.csv的数据提取出来 (如果不允许使用sklearn，规格化可能需要自己写)
-# sklearn的源码，查看classification的结果写法
-# 测试集与训练集分开
-# 生成数据的问题    
+class MushroomProcessing(object):
+    def __init__(self):
+        self.dataset = pd.read_csv("./mushrooms.csv")
+        self.x = self.dataset.drop('class', axis=1)
+        self.y = self.dataset['class']
+        Enconder_x = preprocessing.LabelEncoder()
+        for col in self.x.columns:
+            self.x[col] = Enconder_x.fit_transform(self.x[col])
+        Enconder_y = preprocessing.LabelEncoder()
+        self.y = Enconder_y.fit_transform(self.y)
+        self.x = pd.get_dummies(self.x,columns=self.x.columns,drop_first=True)
+        self.x = np.array(self.x)
+        self.x_scaled = preprocessing.scale(self.x)
+    def get_data(self):
+        return self.x_scaled, self.y
