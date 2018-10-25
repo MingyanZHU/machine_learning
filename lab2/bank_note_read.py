40c4a4cf2b7d5815f14cf2b1799cf55e12d24d94
1160300314 <hiterzmy@outlook.com>
2018-10-25 12:34:30 +0800

    增加Windows相对路径

--- lab2/bank_note_read.py
+++ lab2/bank_note_read.py
@@ -3,19 +3,21 @@ import pandas as pd
 
 
 class BankNoteRead(object):
+    """ 读取钞票数据集信息 """
     def __init__(self):
-        self.data_set = pd.read_csv("./data_banknote_authentication.csv")
+        self.data_set = pd.read_csv("./data_banknote_authentication.csv") # linux 相对路径
+        # self.data_set = pd.read_csv(".\data_banknote_authentication.csv") # windows
         self.x = self.data_set.drop('a', axis=1)
         self.y = self.data_set['a']
     def get_data(self):
         return np.array(self.x, dtype=float), np.array(self.y)
 
-def main():
-    bn = BankNoteRead()
-    print(bn.data_set.head())
-    print(bn.data_set.isnull().sum())
-    print(bn.x.head())
-    print(bn.y.head())
-
-if __name__ == '__main__':
-    main()
+# def main():
+#     bn = BankNoteRead()
+#     print(bn.data_set.head())
+#     print(bn.data_set.isnull().sum())
+#     print(bn.x.head())
+#     print(bn.y.head())
+# 用于测试
+# if __name__ == '__main__':
+#     main()
--- lab2/blood_read.py
+++ lab2/blood_read.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 class BloodProcessing(object):
     def __init__(self):
-        self.data_set = pd.read_csv("./blood.csv") 
+        self.data_set = pd.read_csv("./blood.csv") # linux 相对路径 
         self.x = self.data_set.drop('whether he/she donated blood in March 2007', axis=1)
         self.y = self.data_set['whether he/she donated blood in March 2007']
     def get_data(self):
