f7a60fe52c029471131f17e9b2ab1a45b4cd9f2b
1160300314 <1160300314@stu.hit.edu.cn>
2018-10-28 20:10:16 +0800

    完成cluster初始点选择并重构代码

---
+++ lab3/k_means.py
@@ -0,0 +1,65 @@
+import numpy as np
+import collections
+import random
+
+
+class KMeans(object):
+    def __init__(self, data, k, delta=1e-6):
+        self.data = data
+        self.k = k
+        self.delta = delta
+        self.__data_rows, self.__data_columns = data.shape
+        self.__mu = self.__initial_center_not_random()
+
+    @staticmethod
+    def __euclidean_distance(x1, x2):
+        return np.linalg.norm(x1 - x2)
+
+    def __initial_center_not_random(self):
+        """ 选择彼此距离尽可能远的K个点 """
+        # 随机选第1个初始点
+        mu_0 = np.random.randint(0, self.k) + 1
+        mu = [self.data[mu_0]]
+        # 依次选择与当前mu中样本点距离最大的点作为初始簇中心点
+        for times in range(self.k-1):
+            temp_ans = []
+            for i in range(self.__data_rows):
+                temp_ans.append(np.sum([self.__euclidean_distance(
+                    self.data[i], mu[j]) for j in range(len(mu))]))
+            mu.append(self.data[np.argmax(temp_ans)])
+        return np.array(mu)
+
+    def __k_means(self):
+        times = 0
+        while True:
+            c = collections.defaultdict(list)
+            for i in range(self.__data_rows):
+                dij = [self.__euclidean_distance(
+                    self.data[i], self.__mu[j]) for j in range(self.k)]
+                lambda_j = np.argmin(dij)
+                c[lambda_j].append(self.data[i].tolist())
+
+            new_mu = np.array([np.mean(c[i], axis=0).tolist()
+                               for i in range(self.k)])
+
+            loss = np.sum(self.__euclidean_distance(
+                self.__mu[i], new_mu[i]) for i in range(self.k))
+            if loss > self.delta:
+                self.__mu = new_mu
+            else:
+                break
+
+            print(times)
+            times = times + 1
+            print(self.__mu)
+        return self.__mu, c
+
+    def k_means_random_center(self):
+        """ 随机选择k个顶点作为初始簇中心点 """
+        self.__mu = self.data[random.sample(range(self.__data_rows), self.k)]
+        return self.__k_means()
+
+    def k_means_not_random_center(self):
+        """ 随机选择第一个簇中心点 再选择彼此距离最大的k个顶点作为初始簇中心点 """
+        self.__mu = self.__initial_center_not_random()
+        return self.__k_means()
--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,115 +1,82 @@
 import numpy as np
-import random
 from matplotlib import pyplot as plt
-import collections
+
+import k_means
+
 
 def watermelon_data():
-    watermelon = np.array([[ 0.697  ,0.46 ],
-                         [ 0.774  ,0.376],
-                         [ 0.634  ,0.264],
-                         [ 0.608  ,0.318],
-                         [ 0.556  ,0.215],
-                         [ 0.403  ,0.237],
-                         [ 0.481  ,0.149],
-                         [ 0.437  ,0.211],
-                         [ 0.666  ,0.091],
-                         [ 0.243  ,0.267],
-                         [ 0.245  ,0.057],
-                         [ 0.343  ,0.099],
-                         [ 0.639  ,0.161],
-                         [ 0.657  ,0.198],
-                         [ 0.36   ,0.37 ],
-                         [ 0.593  ,0.042],
-                         [ 0.719  ,0.103],
-                         [ 0.359  ,0.188],
-                         [ 0.339  ,0.241],
-                         [ 0.282  ,0.257],
-                         [ 0.748  ,0.232],
-                         [ 0.714  ,0.346],
-                         [ 0.483  ,0.312],
-                         [ 0.478  ,0.437],
-                         [ 0.525  ,0.369],
-                         [ 0.751  ,0.489],
-                         [ 0.532  ,0.472],
-                         [ 0.473  ,0.376],
-                         [ 0.725  ,0.445],
-                         [ 0.446  ,0.459]])
+    watermelon = np.array([[0.697, 0.46],
+                           [0.774, 0.376],
+                           [0.634, 0.264],
+                           [0.608, 0.318],
+                           [0.556, 0.215],
+                           [0.403, 0.237],
+                           [0.481, 0.149],
+                           [0.437, 0.211],
+                           [0.666, 0.091],
+                           [0.243, 0.267],
+                           [0.245, 0.057],
+                           [0.343, 0.099],
+                           [0.639, 0.161],
+                           [0.657, 0.198],
+                           [0.36, 0.37],
+                           [0.593, 0.042],
+                           [0.719, 0.103],
+                           [0.359, 0.188],
+                           [0.339, 0.241],
+                           [0.282, 0.257],
+                           [0.748, 0.232],
+                           [0.714, 0.346],
+                           [0.483, 0.312],
+                           [0.478, 0.437],
+                           [0.525, 0.369],
+                           [0.751, 0.489],
+                           [0.532, 0.472],
+                           [0.473, 0.376],
+                           [0.725, 0.445],
+                           [0.446, 0.459]])
     return watermelon
 
-def generate_data(means, number, k):
-    """ 生成2维数据 """
-    assert len(means) == k
-    assert len(number) == k
+
+def generate_data(sample_means, sample_number, number_k):
+    """ 生成2维数据
+    :argument number_k k类
+    :argument sample_means k类数据的均值 以list的形式给出 如[[1, 2],[-1, -2], [0, 0]]
+    :argument sample_number k类数据的数量 以list的形式给出 如[10, 20, 30]
+    """
+    assert number_k > 0
+    assert len(sample_means) == number_k
+    assert len(sample_number) == number_k
     cov = [[0.1, 0], [0, 0.1]]
-    data = []
-    for i in range(k):
-        for j in range(number[i]):
-            temp_x1, temp_x2 = np.random.multivariate_normal(
-                [means[i], means[i]], cov
-            )
-            data.append([temp_x1, temp_x2])
-    return np.array(data)
+    sample_data = []
+    for index in range(number_k):
+        for times in range(sample_number[index]):
+            sample_data.append(np.random.multivariate_normal(
+                [sample_means[index][0], sample_means[index][1]], cov).tolist())
+    return np.array(sample_data)
 
-def euclidean_distance(x1, x2):
-    """ 输出2个向量之间的欧式距离 """
-    assert x1.shape == x2.shape
-    return np.linalg.norm(x1 - x2)
-
-def k_means(training_x, k, delta=1e-6):
-    data_rows = training_x.shape[0]
-    mu = training_x[random.sample(range(data_rows), k)]
-    times = 0
-    while True:
-        c = collections.defaultdict(list)
-        for i in range(data_rows):
-            dij = [euclidean_distance(training_x[i], mu[j]) for j in range(k)]
-            lambda_j = np.argmin(dij)
-            c[lambda_j].append(training_x[i].tolist())
-
-        # for i in range(k):
-        #     mu[i] = np.mean(c[i], axis=0)
-        new_mu = np.array([np.mean(c[i],axis=0).tolist() for i in range(k)])
-
-        loss = 0
-        for i in range(k):
-            loss += euclidean_distance(np.array(mu[i]), np.array(new_mu[i]))
-        if loss > delta:
-            mu = new_mu
-        else:
-            break
-
-        # print(times)
-        # times = times + 1
-        # for i in range(k):
-        #     print(mu[i])
-    return mu, c
 
 k = 3
-means = [4, -4, 0]
+means = [[2, 4], [0, -4], [-2, 2]]
 number = [100, 100, 100]
 data = generate_data(means, number, k)
-mu, c = k_means(data, k)
-# ans = []
-# TODO k-means 可能会陷入局部最优解 所以如何选择初始簇中心点
-# 参考 https://blog.csdn.net/zhihaoma/article/details/48649489?utm_source=blogxgwz0
-# 可以选择彼此距离尽可能远的k个点 作为初始簇中心
-# for times in range(200):
-#     print(times)
-#     mu, c = k_means(data, k)
-#     summ = 0
-#     for i in range(k):
-#         temp = [euclidean_distance(np.array(c[i][j]), mu[i]) for j in range(len(c[i]))]
-#         summ += np.sum(temp)
-#     print(summ)
-#     ans.append(summ)
-# print(ans)
-# print(np.min(ans))
-# print(np.max(ans))
-
+km = k_means.KMeans(data, k)
+mu_random, c_random = km.k_means_random_center()
+mu_normal, c_normal = km.k_means_not_random_center()
 # watermelon = watermelon_data()
 # mu, c = k_means(watermelon, k)
-plt.scatter(mu[:,0], mu[:,1], facecolor="none", edgecolor="b", label="center")
+plt.subplot(121)
+plt.title("Choose cluster center randomly")
 for i in range(k):
-    plt.scatter(np.array(c[i])[:,0], np.array(c[i])[:,1], marker="x", label=str(i+1))
+    plt.scatter(np.array(c_random[i])[:, 0], np.array(c_random[i])[:, 1], marker="x", label=str(i + 1))
+plt.scatter(mu_random[:, 0], mu_random[:, 1], facecolor="none", edgecolor="r", label="center")
 plt.legend()
-plt.show() 
+
+plt.subplot(122)
+plt.title("Choose cluster center with each of them max distances")
+for i in range(k):
+    plt.scatter(np.array(c_normal[i])[:, 0], np.array(c_normal[i])[:, 1], marker="x", label=str(i + 1))
+plt.scatter(mu_normal[:, 0], mu_normal[:, 1], facecolor="none", edgecolor="r", label="center")
+plt.legend()
+
+plt.show()
